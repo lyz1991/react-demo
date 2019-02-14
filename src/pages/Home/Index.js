522859c8785e6c3d806eb4f511ea37a593c8c100
lyz1991 <1051500917@qq.com>
2019-02-14 17:59:01 +0800

    update react-hook

--- src/pages/Home/Index.js
+++ src/pages/Home/Index.js
@@ -1,15 +1,20 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import Ads from '../../components/Common/ads'
 import index from './less/index'
-let channel = localStorage.getItem('channel')
 import {
   Link,
 } from 'react-router-dom'
-const Home = ({history, phone, customerId}) => (
-  <div className ={index.header}>
-    <Ads/>
-      <img src="../../static/images/mask/huojian.png"/>
-      <Link to="/personal">去个人页</Link>
-  </div>
-)
+function Home() {
+    const [count, setCount] = useState({age: 22});
+    return (
+     <div className ={index.header}>
+      <Ads/>
+       <p onClick={() => {
+         setCount({age: count.age + 1})
+       }}>次数{ count.age }</p>
+        <img src="../../static/images/mask/huojian.png"/>
+        <Link to="/personal">去个人页</Link>
+    </div>
+    );
+}
 export default Home
